95fe1e216322a984506f20676efd67d5f1711398
Hank Liu <hanke.liu@gmail.com>
2020-09-30 11:08:09 -0400

    set up redux for the app, add reducers and form component

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import ReactDOM  from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+import reducers from './reducers';
 import App from './components/App';
-import {BrowserRouter, Route} from 'react-router-dom';
 import Welcome from './components/Welcome';
 import Signup from './components/auth/signup';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App>
-      <Route path='/' exact component={Welcome}></Route>
-      <Route path='/signup' exact component={Signup}></Route>
-    </App>
-  </BrowserRouter>,
+  <Provider store={createStore(reducers, {})}>
+    <BrowserRouter>
+      <App>
+        <Route path='/' exact component={Welcome}></Route>
+        <Route path='/signup' exact component={Signup}></Route>
+      </App>
+    </BrowserRouter>
+  </Provider>,
   document.querySelector('#root'))
---
+++ src/reducers/auth.js
@@ -0,0 +1,8 @@
+const INTIAL_STATE = {
+  authenticated: '',
+  errorMessage: ''
+}
+
+export default function(state=INTIAL_STATE, action) {
+  return state;
+}
